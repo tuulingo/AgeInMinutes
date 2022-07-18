53654f83292b8a89737f8687f632095387e19c29
tuulingo <oliver.tuuling@tptlive.ee>
2022-07-18 19:12:10 +0300

    Just the whole app

---
+++ app/src/main/java/com/oll/agecalc/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.oll.agecalc
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var dateTextView : TextView? = null
+    private var minutesTextView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        dateTextView = findViewById(R.id.dateTextView)
+        minutesTextView = findViewById(R.id.minutesTextView)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    private fun clickDatePicker(){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(this,{view, year, month, dayOfMonth ->
+
+            val selectedDate = "$year/${month+1}/$dayOfMonth"
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            val theDateinMinutes = sdf.parse(selectedDate).time / 60000000000
+            val currentDateinMinutes = sdf.parse(sdf.format(System.currentTimeMillis())).time / 60000000000
+            val differenceInMinutes = currentDateinMinutes - theDateinMinutes
+            dateTextView?.text = selectedDate
+            minutesTextView?.text = differenceInMinutes.toString()
+        },
+            year,
+            month,
+            day)
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
+
+    }
+}
